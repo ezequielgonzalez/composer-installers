494485277a0e6df8d48dc39cd61f9eb25c46ed78
Fabrizio Branca <fabrizio.branca@aoemedia.de>
2014-01-14 22:43:38 -0800

    Reduced to a bare minimum. Added magento-module and magento-source

--- src/Composer/Installers/MagentoInstaller.php
+++ src/Composer/Installers/MagentoInstaller.php
@@ -4,8 +4,7 @@ namespace Composer\Installers;
 class MagentoInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'theme'   => 'app/design/frontend/{$name}/',
-        'skin'    => 'skin/frontend/default/{$name}/',
-        'library' => 'lib/{$name}/',
+        'module'   => '.modman/{$name}/',
+        'source'    => 'htdocs/'
     );
 }
